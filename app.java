44b772fa610258d910098883cd2bb7b54d175d29
TahNaken <111709968+TahNaken@users.noreply.github.com>
2023-05-30 13:08:30 -0500

    Update and rename README.md to app.java

--- app.java
+++ app.java
@@ -1,3 +1,4 @@
 # -helloworld_jan_22
 this project will allow us to set up our first maven project and build with a Jenkins server
 This application is written in java language.
+This application is java application
